1c7b33da4a41e5bf50acb0d8a65a01348229ddfd
Farhad <farhadmikky@gmail.com>
2024-08-10 20:28:47 +0600

    Practice migration

--- config/database.php
+++ config/database.php
@@ -37,6 +37,14 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],      
+
+          'sqlite2' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => database_path('database2.sqlite'),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
