2f1863977242e45d9f79baa4ce2611ab51c16687
Farhad <farhadmikky@gmail.com>
2024-08-24 19:16:08 +0600

    Practice Eloquent ORM

--- database/migrations/2024_07_29_115349_create_posts_table.php
+++ database/migrations/2024_07_29_115349_create_posts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable();
             $table->string('title');
             $table->text('description');
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\Post;
+use App\Models\User;
+
 // Route::get('/', function () {
 
 
@@ -17,7 +20,58 @@ use Illuminate\Support\Facades\Schema;
    
 // });
 Route::get('/', function () {
-    return view('welcome');
+
+    // return view('welcome');
+
+  // $post = new Post;
+  // $post->title = "Thsi is first title";
+  // $post->description = "Thsi is first description";
+  // $post->save();
+
+  // return "<h1>Post Created.</h1>";
+  // dd(User::where('id', 1));
+    // dd(User::all());
+   
+    // dd(User::select('email')->orderBy('id','desc')->get());
+
+
+  // model updated
+
+  // $user = User::find(1)->delete();
+
+  // $user = User::find(1);
+  // $user->name = "Farhad ";
+  // $user->email = "farhad000@gamil.com";
+
+  // $user->update();
+
+// $user = User::create([
+
+//   'name'=> 'farhad',
+//   'email'=> 'email@gmail.com',
+//   'password'=> '123'
+
+// ]);
+
+// return "<h1>User Updated.</h1>";
+// return "<h1>User delete.</h1>";
+// return "<h1>User create.</h1>";
+
+
+  // dd(User::first()->name);
+  // dd(User::get());
+  // dd(User::orderBy('created_at', 'desc')->first()->id);
+  // $user = User::orderBy('created_at', 'desc')->first();
+  // $user->name = "My name is farhad.";
+
+  // $user->save();
+  foreach(User::orderBy('created_at', 'desc')->get() as $user){
+    echo $user->name;
+   echo "<br>";
+  }
+
+
+
 })->name('home');
 // ->name('home')
 // Route::post('/user', UsersController::class);
